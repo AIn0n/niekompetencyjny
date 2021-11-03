667a486c58e6a436b7a754bf49e5c23f9cebf471
Panzer0 <daniel.pazur@gmail.com>
2021-11-03 23:29:41 +0100

    Added string conversions to the Rectangle and Point classes

--- knapsack_2d/figuresTypes.py
+++ knapsack_2d/figuresTypes.py
@@ -1,12 +1,17 @@
 from dataclasses import dataclass
 
+
 @dataclass(frozen=True, order=True)
 class Point:
-    x :int
-    y :int
+    x: int
+    y: int
+
+    def __str__(self):
+        return f"({self.x}, {self.y})"
+
 
 class Rectangle:
-    def __init__(self, a :Point, width :int, height :int) -> None:
+    def __init__(self, a: Point, width: int, height: int) -> None:
         self.width = width
         self.height = height
 
@@ -18,7 +23,11 @@ class Rectangle:
         self.c = Point(a.x + width, a.y + height)
         self.d = Point(a.x, a.y + height)
 
+    def __str__(self):
+        return f"A - {self.a}, B - {self.b}, C - {self.c}, D = {self.d}"
 
 
 if __name__ == '__main__':
     pnt = Point(1, 3)
+    rect = Rectangle(pnt, 5, 10)
+    print(rect)
