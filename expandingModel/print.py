7271cecc121fb3cbb4570ecc59feaeb33589c028
SzymonGoldberg <szymon.gorka.sgorkas@gmail.com>
2021-11-23 03:07:25 +0100

    working with printing

--- expandingModel/print.py
+++ expandingModel/print.py
@@ -4,9 +4,13 @@ from figureTypes import *
 from genAlgorithm import *
 
 pygame.init()
+pygame.font.init()
+font = pygame.font.SysFont('Comic Sans MS', 30)
 
 display_width = 800
 display_height = 600
+SCALE = 10
+
 
 black = (0, 0, 0)
 white = (255, 255, 255)
@@ -14,6 +18,7 @@ white = (255, 255, 255)
 plot_display = pygame.display.set_mode((display_width, display_height))
 plot_display.fill(white)
 
+area = Rect(Point(0, 0), 80, 80)
 squares = tuple([
     Rect(Point(0, 0), 2, 4), 
     Rect(Point(0, 0), 4, 6), 
@@ -21,19 +26,29 @@ squares = tuple([
     Rect(Point(0, 0), 8, 4),
     Rect(Point(0, 0), 8, 6),
     Rect(Point(0, 0), 4, 10)])
-fitCls = FitnessClass(Rect(Point(0, 0), 80, 80),squares)
-genAlg = GeneticAlgorithm(150, 0.1, 0.2, fitCls)
-genAlg.repeat(3000)
+
+fitCls = FitnessClass(area, squares)
+genAlg = GeneticAlgorithm(300, 0.2, 0.1, fitCls)
+genAlg.repeat(7000)
 bestSpieceMan = max(genAlg.generation, key = lambda x : x.fitness)
 print(bestSpieceMan)
 print(bestSpieceMan.fitness)
 
+pygame.draw.rect(plot_display, black, 
+        (area.a.x * SCALE, area.a.y * SCALE, 
+        (area.width_l + area.width_r) * SCALE, 
+        (area.height_u + area.height_d) * SCALE))
+
 for idx, x in enumerate(bestSpieceMan.chrsom):
     curr = squares[idx].cloneOffset(x)
-    pygame.draw.rect(plot_display, (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)), 
-        (curr.a.x, curr.a.y, 
-        (curr.width_l + curr.width_r), 
-        (curr.height_u + curr.height_d)))
+    color = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
+    texture = font.render(f'{curr.width_l + curr.width_r}, {curr.height_d + curr.height_u}', False, color)
+    plot_display.blit(texture, (area.c.x * SCALE, (area.c.y * SCALE) + idx * 30))
+
+    pygame.draw.rect(plot_display, color, 
+        (curr.a.x * SCALE, curr.a.y * SCALE, 
+        (curr.width_l + curr.width_r) * SCALE, 
+        (curr.height_u + curr.height_d) * SCALE))
 
 while True:
     for event in pygame.event.get():
