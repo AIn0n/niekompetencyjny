d3bc9630a304dfd6dd8cb1666836b724e3070ae1
SzymonGoldberg <szymon.gorka.sgorkas@gmail.com>
2021-12-15 22:55:53 +0100

    working with chromosome classes

---
+++ expandingModel/LocationChromosome.py
@@ -0,0 +1,11 @@
+from abstracts.Chromosome import Chromosome
+from figureTypes import Point
+from random import randint
+
+class LocationChromosome(Chromosome):
+    def __init__(self, size, rY, rX):
+        self.rX, self.rY = rX, rY
+        super().__init__(size)
+
+    def randGene(self):
+        return Point(randint(*self.rX), randint(*self.rY))
