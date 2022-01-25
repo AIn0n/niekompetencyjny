3cf0e8683ffcfddc84e010f94c3c1c062b927d16
SzymonGoldberg <szymon.gorka.sgorkas@gmail.com>
2022-01-25 22:24:32 +0100

    Hotfix: removed bug with more than one pair of doors into one room

--- expandingModel/genAlgorithm.py
+++ expandingModel/genAlgorithm.py
@@ -7,17 +7,19 @@ import copy, itertools
 
 
 class Specimen:
-    def __init__(self, size: int, rX: tuple, rY: tuple) -> None:
+    def __init__(
+        self, size: int, rX: tuple, rY: tuple, connections: int
+    ) -> None:
         self.chrsoms = {
             "location": LocationChromosome(size, rY, rX),
             "rotation": BinaryChromosome(size),
             "expansion": BinaryChromosome(size * 4),
-            "doors": FloatChromosome(size),
+            "doors": FloatChromosome(connections),
         }
         self.fitness = 0
 
-    def getChild(self, chrsom: dict, rX: tuple, rY: tuple):
-        result = Specimen(len(self.chrsoms["location"]), rX, rY)
+    def getChild(self, chrsom: dict, rX: tuple, rY: tuple, connections: int):
+        result = Specimen(len(self.chrsoms["location"]), rX, rY, connections)
         result.chrsoms = chrsom
         return result
 
@@ -26,11 +28,19 @@ class FitnessClass:
     def __init__(self, area: Rect, rooms: tuple) -> None:
         self.area = area
         self.rooms = rooms
+        self.connections = self.buildNeighbourList()
         self.rX = (self.area.a.x, self.area.b.x)
         self.rY = (self.area.a.y, self.area.d.y)
 
+    def buildNeighbourList(self) -> set:
+        result = set()
+        for room in self.rooms:
+            for neighbour in room.neighbours:
+                result.add(frozenset([neighbour, room.name]))
+        return result
+
     def getRndSpecimen(self):
-        return Specimen(len(self.rooms), self.rX, self.rY)
+        return Specimen(len(self.rooms), self.rX, self.rY, len(self.connections))
 
     def getChildren(self, p1: Specimen, p2: Specimen, mut: float) -> list:
         chrsoms1, chrsoms2 = {}, {}
@@ -44,21 +54,26 @@ class FitnessClass:
             chrsoms2[k].mutate(mut)
 
         return (
-            p1.getChild(chrsoms1, self.rX, self.rY),
-            p1.getChild(chrsoms2, self.rX, self.rY),
+            p1.getChild(chrsoms1, self.rX, self.rY, len(self.connections)),
+            p1.getChild(chrsoms2, self.rX, self.rY, len(self.connections)),
         )
 
     def validNeighborsAndGetDoors(
         self, rectangles: list, doorsParams: FloatChromosome
     ):
+        connections = []
         result = []
         for i in range(len(self.rooms)):
             tmp = []
             for n in range(len(self.rooms)):
-                if self.rooms[n].name in self.rooms[i].neighbours or (
-                    self.rooms[n].exit
-                    and not rectangles[n].neighbours(self.area)
-                ):
+                pair = frozenset([self.rooms[n].name, self.rooms[i].name])
+                if (
+                    self.rooms[n].name in self.rooms[i].neighbours
+                    or (
+                        self.rooms[n].exit
+                        and not rectangles[n].neighbours(self.area)
+                    )
+                ) and not pair in connections:
                     commVecs = rectangles[n].neighbours(rectangles[i])
                     if len(commVecs) < 1:
                         return False
@@ -69,7 +84,11 @@ class FitnessClass:
                     )
                     if len(allVecs) < 1:
                         return False
-                    tmp.append(Point.getDoorPlacement(allVecs, doorsParams[n]))
+                    tmp.append(
+                        Point.getDoorPlacement(
+                            allVecs, doorsParams[len(connections)]
+                        )
+                    )
             result.append(tmp)
         return result
 
@@ -131,11 +150,9 @@ class GeneticAlgorithm:
         self.generation = [
             fitnessClass.getRndSpecimen() for x in range(generationSize)
         ]
-        self.mutProb, self.elitarism, self.fitnessClass = (
-            mutProb,
-            elitarism,
-            fitnessClass,
-        )
+        self.mutProb = mutProb
+        self.elitarism = elitarism
+        self.fitnessClass = fitnessClass
 
     def buildNewGeneration(self) -> None:
         for elem in self.generation:
